8b545c46cb4911e8bfb4470f74b25903856a8325
Anmol <guptaanmol9953@gmail.com>
2026-07-13 13:06:57 +0530

    Added Cart,CartItems,Order,OrderItems entities with relationship

---
+++ src/main/java/com/shopease/shopease/model/Order.java
@@ -0,0 +1,28 @@
+package com.shopease.shopease.model;
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Data
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> items;
+
+    private Double totalAmount;
+
+    private String status;
+
+    private LocalDateTime orderDate;
+}
---
+++ src/main/java/com/shopease/shopease/model/OrderItem.java
@@ -0,0 +1,25 @@
+package com.shopease.shopease.model;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "order_items")
+@Data
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private Integer quantity;
+
+    private Double priceAtPurchase;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+}
