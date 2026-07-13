ab991d0b52f6bad53e3ff0540af7aa4df9143381
Anmol <guptaanmol9953@gmail.com>
2026-07-13 12:53:04 +0530

    Added user and product entities

---
+++ src/main/java/com/shopease/shopease/model/Product.java
@@ -0,0 +1,23 @@
+package com.shopease.shopease.model;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "products")
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Double price;
+
+    private Integer stockQuantity;
+
+    private String category;
+}
